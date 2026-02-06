b36694c08bee6d3af953c19de927469b142abfef
David Gonzalez <dcgscolchagua@gmail.com>
2026-02-06 18:45:55 -0300

    Add function to calculate thermal KPIs

---
+++ 04_Analisis_de_Datos_Industriales/src/calculos_termicos.py
@@ -0,0 +1,9 @@
+def calcular_kpis(df):
+    Cp = 4180
+    df["Q_real"] = df["m_hot"] * Cp * (df["T_hot_in"] - df["T_hot_out"])
+    df["eficiencia_termica"] = (
+        (df["T_cold_out"] - df["T_cold_in"]) /
+        (df["T_hot_in"] - df["T_cold_in"])
+    )
+    df["desempeno_relativo"] = df["Q_real"] / df["Q_real"].max()
+    return df
